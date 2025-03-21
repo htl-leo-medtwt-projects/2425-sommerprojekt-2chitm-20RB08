5e825a6c4708e5f687fe0e5d1fdcfc1e6a95b058
Roland Bauer <r.bauer@students.htl-leonding.ac.at>
2025-03-21 15:58:42 +0100

    cave werden dynaisch kreaiert

---
+++ project/scipts/script-play-contentgeneration.js
@@ -0,0 +1,14 @@
+/*****************************
+ * Create caves
+ *****************************/
+let maxCave = 6;
+function createCave(){
+    let s = '';
+
+    for (let i = 0; i < maxCave; i++){
+        s += `<img src="../img/caves/cave${i}.png" alt="cave" class="cave" id="cave${i}">`;
+    }
+
+    document.getElementById('caveBox').innerHTML = s;
+}
+createCave();
