bafc71f6a8366b2f1dead5c2a14b613da736117c
Roland Bauer <r.bauer@students.htl-leonding.ac.at>
2025-03-25 18:25:37 +0100

    Grundvorlage von Loot Ertellet

--- project/scipts/loot.js
+++ project/scipts/loot.js
@@ -2,9 +2,11 @@
  * Loot Class
  ***********************/
 class Loot{
-    constructor(label, deck, img){
+    constructor(name, label, deck, price, img){
+        this.name = name;
         this.label = label
         this.deck = deck;
+        this.price = price
         this.img = img
     }
 }
@@ -12,3 +14,12 @@ class Loot{
 /***********************
  * Loot
  ***********************/
+const loot = [flintlock, spikyHat, spyglass];
+// Weapon
+const flintlock = new Loot('Flintlock', 'weapon', [eyesClosedPunch, luckyHit], 20, '../img/flintlock.png')
+
+// Amor
+const spikyHat = new Loot('Spiky Hat', 'amor', [bash, youTryinSomething], 20, '../img/spikyHat.png')
+
+// Skil
+const spyglass = new Loot('Spyglass', 'skil', [demolish, uhh], 20, '../img/lootCard.png')
