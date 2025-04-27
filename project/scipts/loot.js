679d6c85ef398a924b6d0858e835525c5a65ed2e
20RB08 <r.bauer@students.htl-leonding.ac.at>
2025-04-27 14:42:05 +0200

    Lootcard hinzugefügt

--- project/scipts/loot.js
+++ project/scipts/loot.js
@@ -24,8 +24,10 @@ const brassKnuccles = new Loot('Brass Knuccles', 'weapon', [eyesClosedPunch, luc
 const spikyHat = new Loot('Spiky Hat', 'armor', [bash, youTryinSomething], 7, '../img/lootCard/spikyHat.png')
 const hornedHelm = new Loot('Horned Helm', 'armor', [bash, youTryinSomething], 57, '../img/lootCard/hornedHelm.png')
 const doomPlate = new Loot('Doom Plate', 'armor', [bash, youTryinSomething, bash], 20, '../img/lootCard/doomPlate.png')
+const paperCrown = new Loot('Paper Crown', 'armor', [fireShield, cower], 15, '../img/lootCard/paperCrown.png')
 
 // Skil
 const spyglass = new Loot('Spyglass', 'skil', [demolish, uhh], 26, '../img/lootCard/spyglass.png')
+const twing = new Loot('Twing', 'skil', [eyesClosedPunch, oi], 5, '../img/lootCard/twing.png')
 
-const loot = [flintlock, brassKnuccles, spikyHat, hornedHelm, doomPlate, spyglass];
+const loot = [flintlock, brassKnuccles, spikyHat, hornedHelm, doomPlate, spyglass, twing, paperCrown];
