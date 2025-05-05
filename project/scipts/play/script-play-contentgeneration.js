6bb86d713d302c3441fc6aecf3a70b09264c6893
20RB08 <r.bauer@students.htl-leonding.ac.at>
2025-05-05 16:28:05 +0200

    Es gibt nun einen Schluss wenn man das spiel durchgespielt hat. Es gibt auch credit und wenn man will kann man dass spiel von neu spielen

--- project/scipts/play/script-play-contentgeneration.js
+++ project/scipts/play/script-play-contentgeneration.js
@@ -3,39 +3,54 @@
  *****************************/
 let maxCave = 6;
 function createCave() {
-    let s = '';
+    if (player.level < maxCave) {
+        let s = '';
 
-    // create caves
-    for (let i = 0; i < maxCave; i++) {
-        if (i <= player.level) {
-            s += `<img src="../img/caves/cave${i}.png" alt="cave" class="cave" id="cave${i}" onclick="newGame(${i})">`;
-        } else {
-            s += `<img src="../img/caves/cave${i}.png" alt="cave" class="cave" id="cave${i}">`;
+        // create caves
+        for (let i = 0; i < maxCave; i++) {
+            if (i <= player.level) {
+                s += `<img src="../img/caves/cave${i}.png" alt="cave" class="cave" id="cave${i}" onclick="newGame(${i})">`;
+            } else {
+                s += `<img src="../img/caves/cave${i}.png" alt="cave" class="cave" id="cave${i}">`;
+            }
         }
+
+        document.getElementById('caveBox').innerHTML = s;
+
+        // show player gold
+        document.getElementById('playerGold').innerHTML = `<span class="winnedGold">${player.gold}</span><span>Gold</span>`;
+
+        // map arrows
+        document.getElementById('userView').style.backgroundImage = `url(../img/mapArrows/maparrows${player.level}.png), url(../img/background/careered.jpg)`;
+
+        // hintergrund music änder
+        setTimeout(() => {
+            setBackgroundAudio('../sounds/backgroundMusic/battle.mp3');
+        })
+    } else {
+        // show end
+        console.log('show end');
+        showEnd();
+        // alle caves ichre onclick wegnehme
+        let caves = document.querySelectorAll('.cave'); 
+        for (let i = 0; i < caves.length; i++) {
+            caves[i].onclick = null; 
+        }
+        document.getElementById('backScroll').innerHTML = '<p>Back</p>';
+        // level zurücksetzen
+        player.level--
     }
 
-    document.getElementById('caveBox').innerHTML = s;
-
-    // show player gold
-    document.getElementById('playerGold').innerHTML = `<span class="winnedGold">${player.gold}</span><span>Gold</span>`;
-
-    // map arrows
-    document.getElementById('userView').style.backgroundImage = `url(../img/mapArrows/maparrows${player.level}.png), url(../img/background/careered.jpg)`;
-
-    // hintergrund music änder
-    setTimeout(()=>{
-        setBackgroundAudio('../sounds/backgroundMusic/battle.mp3');
-    })
 }
 createCave();
 
-function goBackToMap(){
+function goBackToMap() {
     // Map neu gestalten
     createCave();
 
     // game field
     doAnimation('#game-field', 'appearFromTop', 2, 'reverse');
-    setTimeout(()=>{
+    setTimeout(() => {
         document.getElementById('game-field').style.top = '-100%';
     }, 2000)
 }
---
+++ project/scipts/play/script-play-end.js
@@ -0,0 +1,46 @@
+/*****************
+ * Schow end
+ ***************/
+function showEnd(){
+    console.log('***** Schluss *****');
+    
+    doAnimation('#end-screen', 'fromBottomToTheTop', 65, 'linear');
+    setTimeout(()=>{
+        document.getElementById('end-screen').style.bottom = '0%';
+    }, 65000)
+
+    // music
+    setBackgroundAudio('../sounds/backgroundMusic/endMusic.mp3');
+}
+
+document.getElementById('end-screen').addEventListener('click', () => {
+    const endScreen = document.getElementById('end-screen');
+
+    // Animation stoppen
+    endScreen.style.animation = 'none';
+
+    // Direkt den `bottom`-Wert setzen
+    endScreen.style.bottom = '0%';
+});
+
+/*****************
+ * Restart game
+ *****************/
+function restartGame(){
+    player = new Player(player.name);
+    console.log('===== Spiel wird neugestarter ======')
+    safePlayer();
+}
+
+/********************
+ * Continue the Game
+ ********************/
+function continueGame(){
+    console.log('===== Spiel geht weiter ======');
+    doAnimation('#end-screen', 'goUp', 2, 'linear');
+    setTimeout(()=>{
+        document.getElementById('end-screen').style.bottom = '100%';
+    }, 2000)
+    createCave();
+    safePlayer();
+}
