b67ee3bb25b2ec7a0ce7f31409f6acb07669fc21
20RB08 <r.bauer@students.htl-leonding.ac.at>
2025-05-12 17:41:41 +0200

    Spielstände kann man nun auch löschen

--- project/scipts/index/conentgeneration.js
+++ project/scipts/index/conentgeneration.js
@@ -14,23 +14,34 @@ function showPlayerselection(){
     let s = '';
     for (let i = 0; i < playerData.players.length; i++){
         if (playerData.players[i] == null){
-            s += `<div class="scroll" onclick="playHero(${i})"><a href="pages/reveal.js-master/tutorial.html">New</a></div>`
+            s += `<div id="option${i}" class="scroll" onclick="select(${i})"><p>New</p></div>`
         } else {
-            s += `<div class="scroll" onclick="playHero(${i})"><a href="pages/reveal.js-master/tutorial.html">Lv.${playerData.players[i].level + 1}</a></div>`
+            s += `<div id="option${i}" class="scroll" onclick="select(${i})"><p>Lv.${playerData.players[i].level + 1}</p></div>`
         }
     }
     document.getElementById('playerSelect').innerHTML = s;
 }
 
 /******************
- * Play Hero
+ * Select
  ********************/
-function playHero(pos) {
-    playerData.playerIndex = pos;
+function select(pos) {
+    // show every playestat
+    document.getElementById('playOrDelet').style.top = '80%';
+    document.getElementById('playOrDelet').style.animation = 'none';
+    document.getElementById('playOrDelet').offsetHeight;
+    document.getElementById('playOrDelet').style.animation = 'appear 1s ';
 
-    // Spieler speichern 
-    localStorage['CC_player'] = JSON.stringify(playerData);
+    // einsetzen
+    let s = `<div class="plate" onclick="playHero(${pos})"><p>Play</p></div>`;
+    if (playerData.players[pos] != null){
+        s += ` <div class="plate" onclick="deletePos(${pos})"><p>Delete</p></div>`
+    }
+    document.getElementById('playOrDelet').innerHTML = s
 
-    // Seite wechseln
-    window.location.href = 'pages/reveal.js-master/tutorial.html';
+    // style
+    for (let i = 0; i < playerData.players.length; i++){
+        document.getElementById(`option${i}`).style.opacity = '0.8';
+    }
+    document.getElementById(`option${pos}`).style.opacity = '1';
 }
--- project/scipts/index/playerSelect.js
+++ project/scipts/index/playerSelect.js
@@ -1,7 +1,7 @@
 let playerData = JSON.parse(localStorage['CC_player'] ?? null);
-if (playerData == null){
+if (playerData == null) {
     let newPlayerData = {
-        playerIndex : 0,
+        playerIndex: 0,
         players: [
             null,
             null,
@@ -9,4 +9,31 @@ if (playerData == null){
         ]
     }
     localStorage['CC_player'] = JSON.stringify(newPlayerData);
+}
+
+/******************
+ * Play Hero
+ ********************/
+function playHero(pos) {
+    playerData.playerIndex = pos;
+
+    // Spieler speichern 
+    localStorage['CC_player'] = JSON.stringify(playerData);
+
+    // Seite wechseln
+    window.location.href = 'pages/reveal.js-master/tutorial.html';
+}
+
+/****************
+ * Delete
+ ***************/
+function deletePos(pos) {
+    playerData.players[pos] = null;
+    document.getElementById('playOrDelet').style.top = '1000%';
+
+    // neu anzeigen 
+    showPlayerselection();
+
+    // speicher
+    localStorage['CC_player'] = JSON.stringify(playerData);
 }
